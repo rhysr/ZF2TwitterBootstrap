2ff00694f10185040bf9905e7508460d2369d316
rhysr <rhysr@mega-city-one.co.uk>
2012-06-15 13:53:56 +0100

    forgot to add control group helper to helper map

--- config/module.config.php
+++ config/module.config.php
@@ -1,8 +1,9 @@
 <?php
 return array(
     'view_manager' => array(
-        'helper_map'               => array(
-            'tbFormLabel' => 'ZF2TwitterBootstrap\Form\View\Helper\FormLabel'
+        'helper_map' => array(
+            'tbFormLabel'    => 'ZF2TwitterBootstrap\Form\View\Helper\FormLabel',
+            'tbControlGroup' => 'ZF2TwitterBootstrap\Form\View\Helper\ControlGroup'
         ),
     ),
 );
