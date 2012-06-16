b549cd6111d5099aaf41162d8b81e2c4069a9608
rhysr <rhysr@mega-city-one.co.uk>
2012-06-16 11:12:26 +0100

    added helper map

--- config/module.config.php
+++ config/module.config.php
@@ -2,6 +2,7 @@
 return array(
     'view_manager' => array(
         'helper_map' => array(
+            'tbAlert'        => 'ZF2TwitterBootstrap\Form\View\Helper\Alert',
             'tbFormLabel'    => 'ZF2TwitterBootstrap\Form\View\Helper\FormLabel',
             'tbControlGroup' => 'ZF2TwitterBootstrap\Form\View\Helper\ControlGroup'
         ),
